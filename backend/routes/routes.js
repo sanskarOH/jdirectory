452976ecf1370fb292c0a1259cf7a4f621f5db9f
sanskarOH <sanskaryeeha@gmail.com>
2023-12-19 02:09:08 -0800

    feat:connected the model to routes and tested

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,21 +1,53 @@
 const express = require('express');
-const Model = require('../model/model')
+const Model = require('../model/model'); 
 const router = express.Router();
 
 module.exports = router;
 
-router.post('/post',(req,res)=>{
-    res.send("Poste API")
-})
+router.post('/post', async (req, res) => {
+    const data = new Model({
+        name: req.body.name,
+        phonenumber: req.body.phonenumber,
+        address: req.body.address
+    });
 
-router.get('/getAll',(re1,res)=>{
-    res.send('Get All API')
-})
+    try {
+        const dataToSave = await data.save();
+        res.status(200).json(dataToSave);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+});
 
-router.patch('/update/:id',(req , res)=>{
-    res.send('Update by ID API')
-})
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await Model.find();
+        res.json(data);
+    }
+    catch(error)
+    {
+        res.status(500).json({message: error.message})
 
-router.delete('/delete/:id',(req, res)=>{
-    res.send('Delete bby ID API')
-})
+    }
+   
+});
+
+router.patch('/update/:id', async (req, res) => {
+    try{
+        const id = req.params.id;
+        const updatedData = req.body;
+        const options = {new : true};
+
+        const result = await Model.findByIdAndUpdate(
+            id, updatedData, options
+        )
+        res.send(result) 
+    }
+    catch(error){
+        res.status(500).json({message : error.message})
+    }
+});
+
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API');
+});
